209832b7b1efd47c9bdde3e7f9907bf219cc70c1
Jake Price <jake@jakeprice.net>
2019-07-14 19:57:35 +0100

    Uploading All The Files

---
+++ DestinyAPITest/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace DestinyAPITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.CharBTN1 = new System.Windows.Forms.Button();
+            this.CharBTN2 = new System.Windows.Forms.Button();
+            this.CharBTN3 = new System.Windows.Forms.Button();
+            this.Gun1 = new System.Windows.Forms.PictureBox();
+            this.Gun2 = new System.Windows.Forms.PictureBox();
+            this.Gun3 = new System.Windows.Forms.PictureBox();
+            this.Sparrow = new System.Windows.Forms.PictureBox();
+            this.Ghost = new System.Windows.Forms.PictureBox();
+            this.Ship = new System.Windows.Forms.PictureBox();
+            this.Embelm = new System.Windows.Forms.PictureBox();
+            this.ClassArmour = new System.Windows.Forms.PictureBox();
+            this.Legs = new System.Windows.Forms.PictureBox();
+            this.Chest = new System.Windows.Forms.PictureBox();
+            this.Arms = new System.Windows.Forms.PictureBox();
+            this.Head = new System.Windows.Forms.PictureBox();
+            this.SubClass = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Gun1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gun2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gun3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Sparrow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ghost)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ship)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Embelm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClassArmour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Legs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Chest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Arms)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SubClass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.InputBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InputBox.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox.ForeColor = System.Drawing.Color.White;
+            this.InputBox.Location = new System.Drawing.Point(159, 464);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(160, 25);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.Text = "JP13#21866";
+            this.InputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.InputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputBox_KeyDown);
+            // 
+            // CharBTN1
+            // 
+            this.CharBTN1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.CharBTN1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.CharBTN1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CharBTN1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CharBTN1.ForeColor = System.Drawing.Color.White;
+            this.CharBTN1.Location = new System.Drawing.Point(114, 495);
+            this.CharBTN1.Name = "CharBTN1";
+            this.CharBTN1.Size = new System.Drawing.Size(250, 37);
+            this.CharBTN1.TabIndex = 2;
+            this.CharBTN1.Text = "Char 1";
+            this.CharBTN1.UseVisualStyleBackColor = false;
+            this.CharBTN1.Click += new System.EventHandler(this.CharBTN1_Click);
+            // 
+            // CharBTN2
+            // 
+            this.CharBTN2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.CharBTN2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.CharBTN2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CharBTN2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CharBTN2.ForeColor = System.Drawing.Color.White;
+            this.CharBTN2.Location = new System.Drawing.Point(114, 538);
+            this.CharBTN2.Name = "CharBTN2";
+            this.CharBTN2.Size = new System.Drawing.Size(250, 37);
+            this.CharBTN2.TabIndex = 3;
+            this.CharBTN2.Text = "Char 2";
+            this.CharBTN2.UseVisualStyleBackColor = false;
+            this.CharBTN2.Click += new System.EventHandler(this.CharBTN2_Click);
+            // 
+            // CharBTN3
+            // 
+            this.CharBTN3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.CharBTN3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.CharBTN3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CharBTN3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CharBTN3.ForeColor = System.Drawing.Color.White;
+            this.CharBTN3.Location = new System.Drawing.Point(114, 581);
+            this.CharBTN3.Name = "CharBTN3";
+            this.CharBTN3.Size = new System.Drawing.Size(250, 37);
+            this.CharBTN3.TabIndex = 4;
+            this.CharBTN3.Text = "Char 3";
+            this.CharBTN3.UseVisualStyleBackColor = false;
+            this.CharBTN3.Click += new System.EventHandler(this.CharBTN3_Click);
+            // 
+            // Gun1
+            // 
+            this.Gun1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Gun1.Location = new System.Drawing.Point(12, 12);
+            this.Gun1.Name = "Gun1";
+            this.Gun1.Size = new System.Drawing.Size(96, 96);
+            this.Gun1.TabIndex = 5;
+            this.Gun1.TabStop = false;
+            // 
+            // Gun2
+            // 
+            this.Gun2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Gun2.Location = new System.Drawing.Point(12, 114);
+            this.Gun2.Name = "Gun2";
+            this.Gun2.Size = new System.Drawing.Size(96, 96);
+            this.Gun2.TabIndex = 6;
+            this.Gun2.TabStop = false;
+            // 
+            // Gun3
+            // 
+            this.Gun3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Gun3.Location = new System.Drawing.Point(12, 216);
+            this.Gun3.Name = "Gun3";
+            this.Gun3.Size = new System.Drawing.Size(96, 96);
+            this.Gun3.TabIndex = 7;
+            this.Gun3.TabStop = false;
+            // 
+            // Sparrow
+            // 
+            this.Sparrow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Sparrow.Location = new System.Drawing.Point(12, 420);
+            this.Sparrow.Name = "Sparrow";
+            this.Sparrow.Size = new System.Drawing.Size(96, 96);
+            this.Sparrow.TabIndex = 9;
+            this.Sparrow.TabStop = false;
+            // 
+            // Ghost
+            // 
+            this.Ghost.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Ghost.Location = new System.Drawing.Point(12, 318);
+            this.Ghost.Name = "Ghost";
+            this.Ghost.Size = new System.Drawing.Size(96, 96);
+            this.Ghost.TabIndex = 8;
+            this.Ghost.TabStop = false;
+            // 
+            // Ship
+            // 
+            this.Ship.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Ship.Location = new System.Drawing.Point(12, 522);
+            this.Ship.Name = "Ship";
+            this.Ship.Size = new System.Drawing.Size(96, 96);
+            this.Ship.TabIndex = 10;
+            this.Ship.TabStop = false;
+            // 
+            // Embelm
+            // 
+            this.Embelm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Embelm.Location = new System.Drawing.Point(370, 522);
+            this.Embelm.Name = "Embelm";
+            this.Embelm.Size = new System.Drawing.Size(96, 96);
+            this.Embelm.TabIndex = 16;
+            this.Embelm.TabStop = false;
+            // 
+            // ClassArmour
+            // 
+            this.ClassArmour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.ClassArmour.Location = new System.Drawing.Point(370, 420);
+            this.ClassArmour.Name = "ClassArmour";
+            this.ClassArmour.Size = new System.Drawing.Size(96, 96);
+            this.ClassArmour.TabIndex = 15;
+            this.ClassArmour.TabStop = false;
+            // 
+            // Legs
+            // 
+            this.Legs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Legs.Location = new System.Drawing.Point(370, 318);
+            this.Legs.Name = "Legs";
+            this.Legs.Size = new System.Drawing.Size(96, 96);
+            this.Legs.TabIndex = 14;
+            this.Legs.TabStop = false;
+            // 
+            // Chest
+            // 
+            this.Chest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Chest.Location = new System.Drawing.Point(370, 216);
+            this.Chest.Name = "Chest";
+            this.Chest.Size = new System.Drawing.Size(96, 96);
+            this.Chest.TabIndex = 13;
+            this.Chest.TabStop = false;
+            // 
+            // Arms
+            // 
+            this.Arms.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Arms.Location = new System.Drawing.Point(370, 114);
+            this.Arms.Name = "Arms";
+            this.Arms.Size = new System.Drawing.Size(96, 96);
+            this.Arms.TabIndex = 12;
+            this.Arms.TabStop = false;
+            // 
+            // Head
+            // 
+            this.Head.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.Head.Location = new System.Drawing.Point(370, 12);
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(96, 96);
+            this.Head.TabIndex = 11;
+            this.Head.TabStop = false;
+            // 
+            // SubClass
+            // 
+            this.SubClass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(148)))), ((int)(((byte)(148)))));
+            this.SubClass.Location = new System.Drawing.Point(159, 12);
+            this.SubClass.Name = "SubClass";
+            this.SubClass.Size = new System.Drawing.Size(160, 160);
+            this.SubClass.TabIndex = 18;
+            this.SubClass.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.ClientSize = new System.Drawing.Size(477, 630);
+            this.Controls.Add(this.SubClass);
+            this.Controls.Add(this.Embelm);
+            this.Controls.Add(this.ClassArmour);
+            this.Controls.Add(this.Legs);
+            this.Controls.Add(this.Chest);
+            this.Controls.Add(this.Arms);
+            this.Controls.Add(this.Head);
+            this.Controls.Add(this.Ship);
+            this.Controls.Add(this.Sparrow);
+            this.Controls.Add(this.Ghost);
+            this.Controls.Add(this.Gun3);
+            this.Controls.Add(this.Gun2);
+            this.Controls.Add(this.Gun1);
+            this.Controls.Add(this.CharBTN3);
+            this.Controls.Add(this.CharBTN2);
+            this.Controls.Add(this.CharBTN1);
+            this.Controls.Add(this.InputBox);
+            this.MaximumSize = new System.Drawing.Size(493, 669);
+            this.MinimumSize = new System.Drawing.Size(493, 669);
+            this.Name = "Form1";
+            this.Text = "Loadout Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Gun1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gun2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gun3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Sparrow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ghost)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ship)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Embelm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClassArmour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Legs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Chest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Arms)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SubClass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button CharBTN1;
+        private System.Windows.Forms.Button CharBTN2;
+        private System.Windows.Forms.Button CharBTN3;
+        private System.Windows.Forms.PictureBox Gun1;
+        private System.Windows.Forms.PictureBox Gun2;
+        private System.Windows.Forms.PictureBox Gun3;
+        private System.Windows.Forms.PictureBox Sparrow;
+        private System.Windows.Forms.PictureBox Ghost;
+        private System.Windows.Forms.PictureBox Ship;
+        private System.Windows.Forms.PictureBox Embelm;
+        private System.Windows.Forms.PictureBox ClassArmour;
+        private System.Windows.Forms.PictureBox Legs;
+        private System.Windows.Forms.PictureBox Chest;
+        private System.Windows.Forms.PictureBox Arms;
+        private System.Windows.Forms.PictureBox Head;
+        private System.Windows.Forms.PictureBox SubClass;
+    }
+}
+
---
+++ DestinyAPITest/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace DestinyAPITest
+{
+    public partial class Form1 : Form
+    {
+        WebClient webClient = new WebClient();
+
+        int membershipType = 0;
+        long membershipId = 0;
+
+        List<long> characters = new List<long>();
+
+        JObject objectsItem = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webClient.Headers.Add("X-API-Key", "70c787d071c742d686dfbc7681440cd1");
+
+            activateCharBtns();
+
+            string urlItem = "https://destiny.plumbing/en/raw/DestinyInventoryItemDefinition.json";
+            var jsonItem = webClient.DownloadString(urlItem);
+            objectsItem = JObject.Parse(jsonItem);
+        }
+        void activateCharBtns()
+        {
+            if (characters.Count == 0)
+            {
+                CharBTN1.Visible = false;
+                CharBTN2.Visible = false;
+                CharBTN3.Visible = false;
+            }
+            if (characters.Count == 1)
+            {
+                CharBTN1.Visible = true;
+                CharBTN2.Visible = false;
+                CharBTN3.Visible = false;
+            }
+            if (characters.Count == 2)
+            {
+                CharBTN1.Visible = true;
+                CharBTN2.Visible = true;
+                CharBTN3.Visible = false;
+            }
+            if (characters.Count == 3)
+            {
+                CharBTN1.Visible = true;
+                CharBTN2.Visible = true;
+                CharBTN3.Visible = true;
+            }
+
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && InputBox.TextLength > 0)
+            {
+                searchUsers(InputBox.Text);
+            }
+        }
+
+        void searchUsers(string username)
+        {
+            string prepedUser = username.Replace("#", "%23");
+            string url = "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/" + prepedUser + "/";
+
+            var json = webClient.DownloadString(url);
+            var objects = JObject.Parse(json);
+
+            if (objects.SelectToken("Response").HasValues == true)
+            {
+                membershipType = (int)objects.SelectToken("Response[0].membershipType");
+                membershipId = (long)objects.SelectToken("Response[0].membershipId");
+
+
+                string url2 = "https://www.bungie.net/Platform/Destiny2/" + membershipType + "/Profile/" + membershipId + "/?components=100";
+
+                var json2 = webClient.DownloadString(url2);
+                var objects2 = JObject.Parse(json2);
+
+                if (objects2.SelectToken("Response").HasValues == true)
+                {
+                    characters.Clear();
+                    IEnumerable<JToken> list = objects2.SelectTokens("Response.profile.data.characterIds[*]");
+                    foreach (var item in list)
+                    {
+                        characters.Add(Convert.ToInt64(item));
+                    }
+                    activateCharBtns();
+
+                    int loopCount = 0;
+                    foreach (var item in characters)
+                    {
+                        string url3 = "https://www.bungie.net/Platform/Destiny2/" + membershipType + "/Profile/" + membershipId + "/Character/" + item + "/?components=200";
+                        var json3 = webClient.DownloadString(url3);
+                        var objects3 = JObject.Parse(json3);
+
+                        if (objects3.SelectToken("Response").HasValues == true)
+                        {
+                            string info = classLookUp((int)objects3.SelectToken("Response.character.data.classType")) + " : " + ((int)objects3.SelectToken("Response.character.data.light")).ToString();
+                            charsBTN(info, loopCount);
+                        }
+                        else
+                        {
+                            errorShow();
+                        }
+                        loopCount++;
+                    }
+                }
+                else
+                {
+                    errorShow();
+                }
+            }
+            else
+            {
+                errorShow();
+            }
+        }
+
+        string classLookUp(int val)
+        {
+            switch (val)
+            {
+                case 0:
+                    return "Titan";
+                    break;
+                case 1:
+                    return "Hunter";
+                    break;
+                case 2:
+                    return "Warlock";
+                    break;
+            }
+            return null;
+        }
+
+        void charsBTN(string info, int val)
+        {
+            switch (val)
+            {
+                case 0:
+                    CharBTN1.Text = info;
+                    break;
+                case 1:
+                    CharBTN2.Text = info;
+                    break;
+                case 2:
+                    CharBTN3.Text = info;
+                    break;
+            }
+        }
+
+        void errorShow()
+        {
+
+        }
+
+        private void CharBTN1_Click(object sender, EventArgs e)
+        {
+            showCharLoadout(0);
+        }
+
+        private void CharBTN2_Click(object sender, EventArgs e)
+        {
+            showCharLoadout(1);
+        }
+
+        private void CharBTN3_Click(object sender, EventArgs e)
+        {
+            showCharLoadout(2);
+        }
+
+        void showCharLoadout(int val)
+        {
+            string url = "https://www.bungie.net/Platform/Destiny2/" + membershipType + "/Profile/" + membershipId + "/Character/" + characters[val] + "/?components=205";
+
+            var json = webClient.DownloadString(url);
+            var objects = JObject.Parse(json);
+
+            if (objects.SelectToken("Response").HasValues == true)
+            {
+                for (int i = 0; i < objects.SelectToken("Response.equipment.data.items").Count(); i++)
+                {
+                    if (i != 14 || i != 12)
+                    {
+                        uint itemId = (uint)objects.SelectToken("Response.equipment.data.items[" + i + "].itemHash");
+
+                        searchItemDef(itemId, i);
+                    }
+                }
+            }
+            else
+            {
+                errorShow();
+            }
+        }
+
+        void searchItemDef(uint itemID, int loopCount)
+        {
+            Task<string> itemTask = Task.Run(() => getItemDef(itemID));
+
+            itemImgSetter(loopCount, itemTask.Result);
+        }
+
+        Task<string> getItemDef(uint itemID)
+        {
+            return Task.FromResult("https://www.bungie.net" + (string)objectsItem.SelectToken(itemID + ".displayProperties.icon"));
+        }
+
+        void itemImgSetter(int val, string imgPath)
+        {
+            switch (val)
+            {
+                case 0:
+                    Gun1.Load(imgPath);
+                    break;
+                case 1:
+                    Gun2.Load(imgPath);
+                    break;
+                case 2:
+                    Gun3.Load(imgPath);
+                    break;
+                case 3:
+                    Head.Load(imgPath);
+                    break;
+                case 4:
+                    Arms.Load(imgPath);
+                    break;
+                case 5:
+                    Chest.Load(imgPath);
+                    break;
+                case 6:
+                    Legs.Load(imgPath);
+                    break;
+                case 7:
+                    ClassArmour.Load(imgPath);
+                    break;
+                case 8:
+                    Ghost.Load(imgPath);
+                    break;
+                case 9:
+                    Sparrow.Load(imgPath);
+                    break;
+                case 10:
+                    Ship.Load(imgPath);
+                    break;
+                case 11:
+                    SubClass.Load(imgPath);
+                    break;
+                case 13:
+                    Embelm.Load(imgPath);
+                    break;
+            }
+        }
+    }
+}
